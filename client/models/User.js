57e7e4b9162f56ef45b9c2cc8377af79dee2bb9f
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-15 21:16:42 +1100

    added the user schema and install mongoose

---
+++ client/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    _id: { type: Number },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true }, 
+    phone: { type: String, required: true },
+});
+
+userSchema.plugin(generateUniqueKey(() => String(Math.floor(Math.random() * 1000000))));
+const User = mongoose.model("User", userSchema);
+
+module.export = User;
