e3412b84a0d1a61051f67272f7f2b4920a750907
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-19 19:02:28 +1100

    add the row and col under the jumboTron

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import JumboHome from '../components/JumboHome/JumboHome'
+import JumboHome from '../components/JumboHome/JumboHome';
+import {  Container, Row, Col } from 'react-bootstrap';
 
 
 
-function Home () {
+function Home() {
 
     return (
         <>
-         <h1 style={{fontSize: "100px"}}>Hello </h1>
-        <JumboHome />
-       
+            <h1 style={{ fontSize: "100px" }}>Hello </h1>
+            <JumboHome />
+            <Container>
+                <Row>
+                    <Col>Blog</Col>
+                    <Col>Make A booking</Col>
+                    <Col>Create Photographer Profile</Col>
+                </Row>
+            </Container>
+
         </>
     )
 }
