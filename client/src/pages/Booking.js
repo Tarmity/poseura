42db1f4dd8ca5f7fec16a198b9f94c698f260c0e
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-19 19:09:31 +1100

    added the blank module layout

---
+++ client/src/pages/Booking.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { } from 'react-bootstrap';
+
+
+class Booking extends Component {
+
+    render () {
+        return(
+
+
+        )
+    }
+}
+
+export default Booking; 
