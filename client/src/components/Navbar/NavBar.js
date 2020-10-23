a365bea6990265ac457994330575ad1efa083f97
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-23 20:35:02 +1100

    added the new logo to the Navbar

--- client/src/components/Navbar/NavBar.js
+++ client/src/components/Navbar/NavBar.js
@@ -13,12 +13,12 @@ export default function NavBar () {
         <Navbar.Brand href="/home">
           <img
             alt=""
-            src= "https://github.com/Tarmity/poseura/blob/main/client/src/components/Images/logo.png?raw=true"
-            width="30"
-            height="30"
+            src= "https://github.com/Tarmity/poseura/blob/main/client/src/components/Images/Logo2.png?raw=true"
+            width="80"
+            height="40"
             className="d-inline-block align-top"
           />{' '}
-          Poseura
+     
         </Navbar.Brand>
         <Link style={{ color: '#FF5E0E' }} to="/home">Home</Link>
       </Navbar>
