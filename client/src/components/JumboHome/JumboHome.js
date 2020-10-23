54ee96f9f1be59f5ee7bc6a5b0c58c16a1be70c5
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-23 20:49:07 +1100

    added the glass effect behind the writing

--- client/src/components/JumboHome/JumboHome.js
+++ client/src/components/JumboHome/JumboHome.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import {  Container, Jumbotron } from 'react-bootstrap';
+import { Container, Jumbotron } from 'react-bootstrap';
 import './JumboHome.css';
 
 
 
-function JumboHome () {
+function JumboHome() {
 
 
-    
-         return(
 
-            <Jumbotron  className= "jumbo" fluid >
-            <Container>
-              <h1 className="header">Welcome to Poseur</h1>
-              <p className= "para">
-                A community photographers capturing moments in time to cherish
+  return (
+
+    <Jumbotron className="jumbo" fluid >
+      <Container
+        style={{
+          backgroundColor: '#454545',
+          textAlign: 'center',
+          width: '800px',
+          opacity: '0.6',
+        }}>
+        <h1 className="header">Welcome to Poseur</h1>
+        <p className="para">
+          A community of photographers capturing moments in time to cherish
               </p>
-            </Container>
-          </Jumbotron>
+      </Container>
+    </Jumbotron>
 
-         )
-   
+  )
+
 }
 
 export default JumboHome;
